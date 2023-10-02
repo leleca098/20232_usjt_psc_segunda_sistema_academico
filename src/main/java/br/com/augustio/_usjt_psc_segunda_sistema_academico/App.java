81c63f17731b739863a8181d97d85343c18808db
Leticia Augusto <leticiaaugusto97@gmail.com>
2023-10-02 20:58:27 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/augustio/_usjt_psc_segunda_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.augustio._usjt_psc_segunda_sistema_academico;
+
+/**
+ *
+ * @author leticia
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
